3f80fbe0609c7b83510dd9cd79902e76796b4ed6
Jimmy Sossa <sossajimmy8@gmail.com>
2024-01-07 19:52:56 -0500

    jun

--- src/components/LocationCard.jsx
+++ src/components/LocationCard.jsx
@@ -1,13 +1,16 @@
 
 const LocationCard = ({ location }) => {
- <article>
-    <h2>{location?.name}</h2>
-    <ul>
-        <li><span>Type:</span><span>{location?.type}</span></li>
-        <li><span>Dimension:</span><span>{location?.dimension}</span></li>
-        <li><span>Population:</span><span>{location?.residents.length}</span></li>
-    </ul>
- </article>
+    return (
+        <article>
+        <h2>{location?.name}</h2>
+        <ul>
+            <li><span>Type:</span><span>{location?.type}</span></li>
+            <li><span>Dimension:</span><span>{location?.dimension}</span></li>
+            <li><span>Population:</span><span>{location?.residents.length}</span></li>
+        </ul>
+     </article>
+    )
+
 }
 
 export default LocationCard
